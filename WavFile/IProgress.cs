03a9e44c4ec17779003efd792d60030e7fc61761
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2015-03-28 13:19:09 +0100

    Update IProgress.cs

--- WavFile/IProgress.cs
+++ WavFile/IProgress.cs
@@ -1,7 +1,5 @@
-﻿/*
- * WavFile
- * Copyright © Wiesław Šoltés 2010-2012. All Rights Reserved
- */
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace WavFile
 {
