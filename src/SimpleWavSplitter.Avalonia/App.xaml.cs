90e20643901cfafd212e8b374f73bc0f36471f75
wieslawsoltes <wieslaw.soltes@gmail.com>
2020-08-18 05:38:07 +0000

    Updated to 0.10.0-preview3

--- src/SimpleWavSplitter.Avalonia/App.xaml.cs
+++ src/SimpleWavSplitter.Avalonia/App.xaml.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Logging.Serilog;
 using Avalonia.Markup.Xaml;
 
 namespace SimpleWavSplitter.Avalonia
